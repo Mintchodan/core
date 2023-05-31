2d7267ab39a427b43c9b111653b389252c04d14c
Mintchodan <z78z@naver.com>
2023-05-31 20:27:38 +0900

    fix: AppConfig
    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository()); // 멤버 서비스의 구현을 MemberServiceImpl 을 쓴다는걸 알 수 있음
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() { // 멤버 리포지토리는 MemoryMemberRepository 를 쓴다는걸 알 수 있음
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() { // 오더 서비스의 리포지토리는 MemoryMemberRepository 를 쓰고
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
