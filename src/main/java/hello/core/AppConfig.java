3d9bb9cbfb4c00d69d59637eaa6dbe61e1e1c24d
Mintchodan <z78z@naver.com>
2023-05-31 21:33:07 +0900

    feat: 프로젝트 Spring 전환
    전체적인 코드를 Spring을 사용하여 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 멤버 서비스의 구현을 MemberServiceImpl 을 쓴다는걸 알 수 있음
     }
 
-    private MemberRepository memberRepository() { // 멤버 리포지토리는 MemoryMemberRepository 를 쓴다는걸 알 수 있음
+    @Bean
+    public MemberRepository memberRepository() { // 멤버 리포지토리는 MemoryMemberRepository 를 쓴다는걸 알 수 있음
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() { // 오더 서비스의 리포지토리는 MemoryMemberRepository 를 쓰고
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AppConfig의 Bean에 들어있는 모든 객체를 관리하는 객체
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 파라미터엔 Bean에 있는 불러올 메서드 이름과 타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP); // id가 1L은 id타입이 Long타입이라 L 붙여야 함
         memberService.join(member);
